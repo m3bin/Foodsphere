17ffa4919437d5caff311db43e3f8190c0f0679f
Mebin Mathew <30719660+m3bin@users.noreply.github.com>
2018-02-07 15:51:38 +0530

    image upload successful

--- app/src/main/java/com/project/foodsphere/sell_activity.java
+++ app/src/main/java/com/project/foodsphere/sell_activity.java
@@ -1,21 +1,33 @@
 package com.project.foodsphere;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class sell_activity extends AppCompatActivity {
 
     Button uploadbtn, choosebtn;
+    String pid;
     private Uri filePath;
     private final int PICK_IMAGE_REQUEST = 71;
 
@@ -24,7 +36,15 @@ public class sell_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_activity);
 
-
+        //image choose
+        choosebtn = (Button) findViewById(R.id.prod_btnChoose);
+        choosebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseimage();
+            }
+        });
+        //upload code
         uploadbtn = (Button) findViewById(R.id.prod_btn);
         uploadbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,9 +79,70 @@ public class sell_activity extends AppCompatActivity {
                 products.put("measurement", pmeasurement);
                 products.put("quantity", pquantity);
 
-                String pid = ref.push().getKey();
+                pid = ref.push().getKey();
                 ref.child(pid).setValue(products);
+
+               uploadImage();
+
             }
         });
+    }
+
+    public void chooseimage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+
+        }
+    }
+
+    private void uploadImage() {
+
+        FirebaseStorage storage;
+        StorageReference storageReference;
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("images/"+ pid.toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(sell_activity.this, "Upload Successful ", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(sell_activity.this, "Upload Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
     }
 }
